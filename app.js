b17856b01cc45decfe30bd2245dbfaded7837a41
chirag4582 <115335660+chirag4582@users.noreply.github.com>
2022-10-20 15:35:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const container = document.getElementsByClassName("container")
+
+const element = document.getElementsByClassName('elements')
+
+for(i=0; i<element.length ; i++)
+{console.log(element[i]);}
